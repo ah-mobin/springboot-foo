f231d182f4e72a975c8d209123954b64dd9a7a4c
mobin-newroz <ahmobin@newroztech.com>
2022-12-05 13:05:32 +0600

    feat : java annotation based setter injection

---
+++ src/main/java/com/foo/springfooproject/BeanConfig.java
@@ -0,0 +1,31 @@
+package com.foo.springfooproject;
+
+import models.Coder;
+import models.Computer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public Coder setterInjCoder(@Qualifier("computerSetterBean") Computer computer){
+        Coder coder = new Coder();
+        coder.setId(1036);
+        coder.setName("Abu Horaira");
+        coder.setLanguage("Java");
+        coder.setComputer(computer);
+        return coder;
+    }
+
+    @Bean
+    @Qualifier("computerSetterBean")
+    public Computer setterInjComputer(){
+        Computer computer = new Computer();
+        computer.setBrand("AMD");
+        return computer;
+    }
+
+
+}
--- src/main/java/com/foo/springfooproject/SpringFooProjectApplication.java
+++ src/main/java/com/foo/springfooproject/SpringFooProjectApplication.java
@@ -4,16 +4,18 @@ import models.Coder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath:BeanConfig.xml"})
+@ImportResource({"classpath:BeanConfig.xml"}) // to import bean configs for xml based injection
+@Import(BeanConfig.class) // to import bean configs for java based injection
 public class SpringFooProjectApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringFooProjectApplication.class, args);
-        Coder coder = (Coder) context.getBean("coder1");
-        Coder coder2 = (Coder) context.getBean("coder2");
+        Coder coder = (Coder) context.getBean("setterInjCoder");
+//        Coder coder2 = (Coder) context.getBean("coder2");
         System.out.println(coder.getName());
         System.out.println(coder.getLanguage());
         System.out.println(coder.getComputer().getBrand());
